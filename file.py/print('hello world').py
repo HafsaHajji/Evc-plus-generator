c06717a89d37835003a7ef75173ca2c189aa1860
Hafsa Hassan <xafsaxaaji936@gmail.com>
2024-12-20 09:57:05 +0300

    this is EVC Plus in python

---
+++ file.py/print('hello world').py
@@ -0,0 +1,18 @@
+#print('hafsa')
+#EVC PLUS
+pin = int(input("EVC PLUS Fadlam Gali Pin-kaga (Enter PIN)"))
+if pin == 2233:
+   g = int(input(print("1. Itus Haraagaga  \n2. Kaarka Hadalka \n3. bixi Biil \n4. U wareeji EVCPlus \n5. Warbixin Kooban \n6. Salaam Bank \n7 Mareeynta \n8. TAAJ \n0. Ka Bax")))
+   if g == 1:
+    print("Haragaagu waa $10")
+   elif 2:
+    print("1. ku shubo Ku hadalka Gudaha \n2. Ugu Shubo ku hadalka Dibada")
+   elif 3:
+    print("1. Post Paid \n2. Ku iibso")
+else:
+    print("Pinka aad galisay waa qalad")
+    
+
+
+
+
